297bf88ccd9993a52bc44e286524ba62e67a5723
Hovo-93 <ovo.aroyan@mail.ru>
2023-07-13 15:30:07 +0400

    added new settings

--- analogue_character_ai_telegram/analogue_character_ai_telegram/settings.py
+++ analogue_character_ai_telegram/analogue_character_ai_telegram/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,8 +26,14 @@ SECRET_KEY = 'django-insecure-te%8#cg^y^x2%kdtmwd&rnem4hnd^(+c8a3zc&#1g1n@+9$tf9
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
+# CORS_ORIGIN_WHITELIST = [
+#     'https://9a10-185-177-104-162.ngrok-free.app',
+#     # Add other trusted origins if needed
+# ]
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://9a10-185-177-104-162.ngrok-free.app',
+# ]
+CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,18 +43,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'character_ai',
+
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 ]
+
 
 ROOT_URLCONF = 'analogue_character_ai_telegram.urls'
 
@@ -115,11 +128,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'character_ai.User'
+AUTH_USER_MODEL = 'character_ai.TelegramUser'
+
