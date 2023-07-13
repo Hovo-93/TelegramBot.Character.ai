b18f9f14f74702424615608eecd55ba1ebb0543d
Hovo-93 <ovo.aroyan@mail.ru>
2023-07-13 15:30:32 +0400

    added models to admin

--- analogue_character_ai_telegram/character_ai/admin.py
+++ analogue_character_ai_telegram/character_ai/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User, Character
+from character_ai.models import TelegramUser, Character
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(TelegramUser)
 
 admin.site.register(Character)
